111f03ae2ae6466e5408e5a5ca3b2aacd5402f06
Tiago Sampaio <tiago@tiagosampaio.com>
2018-12-13 18:02:55 -0200

    Added new class to test.

---
+++ dev/test/unit/Frenet/ObjectType/Entity/Shipping/Info/ServiceTest.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace FrenetTest\ObjectType\Entity\Shipping\Info;
+
+use FrenetTest\TestCase;
+
+/**
+ * Class ServiceTest
+ *
+ * @package FrenetTest\ObjectType\Entity\Shipping\Info
+ */
+class ServiceTest extends TestCase
+{
+    /**
+     * @var \Frenet\ObjectType\Entity\Shipping\Info\ServiceInterface
+     */
+    private $object;
+    
+    /**
+     * @var array
+     */
+    private $data = [
+        'ServiceCode' => 'UPS',
+        'ServiceDescription' => 'Shipping with UPS Service.',
+        'Carrier' => 'SomeCarrier',
+        'CarrierCode' => 'SomeCarrierCode',
+    ];
+    
+    protected function setUp()
+    {
+        $this->object = $this->createObject(\Frenet\ObjectType\Entity\Shipping\Info\ServiceInterface::class, [
+            'data' => $this->data
+        ]);
+    }
+    
+    /**
+     * @test
+     */
+    public function getServiceCode()
+    {
+        $this->assertEquals('UPS', $this->object->getServiceCode());
+    }
+    
+    /**
+     * @test
+     */
+    public function getServiceDescription()
+    {
+        $this->assertEquals('Shipping with UPS Service.', $this->object->getServiceDescription());
+    }
+    
+    /**
+     * @test
+     */
+    public function getCarrier()
+    {
+        $this->assertEquals('SomeCarrier', $this->object->getCarrier());
+    }
+    
+    /**
+     * @test
+     */
+    public function getCarrierCode()
+    {
+        $this->assertEquals('SomeCarrierCode', $this->object->getCarrierCode());
+    }
+}
