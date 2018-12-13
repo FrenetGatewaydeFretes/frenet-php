48587bddcce0ae61293fb77a25a297d8d42f1cc2
Tiago Sampaio <tiago@tiagosampaio.com>
2018-12-13 20:47:59 -0200

    Added new unit test.

---
+++ dev/test/unit/Frenet/Model/Normalizer/PostcodeNormalizerTest.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace FrenetTest\Model\Normalizer;
+
+use FrenetTest\TestCase;
+
+/**
+ * Class PostcodeNormalizer
+ *
+ * @package FrenetTest\Model\Normalizer
+ */
+class PostcodeNormalizerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function normalize()
+    {
+        /** @var \Frenet\Model\Normalizer\PostcodeNormalizer $model */
+        $model = $this->createObject(\Frenet\Model\Normalizer\PostcodeNormalizer::class);
+        $this->assertEquals('06395010', $model->normalize('06395-010'));
+        $this->assertEquals('06395010', $model->normalize('6395-010'));
+        $this->assertEquals('06395010', $model->normalize('6395010'));
+        $this->assertEquals('06395010', $model->normalize('6395--------010'));
+        $this->assertEquals('06395010', $model->normalize('6395--------010 '));
+        $this->assertEquals('06395010', $model->normalize(' 6395-010 '));
+        $this->assertEquals('06395010', $model->normalize('6a3b9c5d0e1f0g'));
+        $this->assertNotEquals('06395010', $model->normalize('06395-001'));
+        $this->assertNull($model->normalize(''));
+    }
+}
