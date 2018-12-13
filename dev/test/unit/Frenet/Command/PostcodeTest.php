8face59e30303a182c02125d01dced3a3d613afe
Tiago Sampaio <tiago@tiagosampaio.com>
2018-12-13 19:05:01 -0200

    Added new unit test.

---
+++ dev/test/unit/Frenet/Command/PostcodeTest.php
@@ -0,0 +1,38 @@
+<?php
+
+declare(strict_types = 1);
+
+namespace FrenetTest\Command;
+
+use FrenetTest\TestCase;
+
+/**
+ * Class PostcodeTest
+ *
+ * @package FrenetTest\Command
+ */
+class PostcodeTest extends TestCase
+{
+    /**
+     * @var \Frenet\Command\PostcodeInterface
+     */
+    private $object;
+    
+    protected function setUp()
+    {
+        $this->object = $this->createObject(\Frenet\Command\PostcodeInterface::class);
+    }
+    
+    /**
+     * @test
+     */
+    public function address()
+    {
+        /**
+         * @var \Frenet\Command\Postcode\AddressInterface
+         */
+        $address = $this->object->address('06395-010');
+        $this->assertInstanceOf(\Frenet\Command\Postcode\AddressInterface::class, $address);
+        $this->assertEquals('06395-010', $address->getPostcode());
+    }
+}
