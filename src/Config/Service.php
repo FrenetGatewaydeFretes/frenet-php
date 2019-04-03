18980fb2f0cf0de22fef1db9dd5845fde0d2a24b
Tiago Sampaio <tiago@tiagosampaio.com>
2019-04-03 12:10:29 -0300

    Added the ability to define a custom protocol and hostname for service configuration.

--- src/Config/Service.php
+++ src/Config/Service.php
@@ -10,6 +10,13 @@ namespace Frenet\Config;
 class Service implements ConfigInterface
 {
     /**
+     * @var array
+     */
+    private $validProtocols = [
+        'http', 'https'
+    ];
+
+    /**
      * @var string
      */
     private $protocol = 'http';
@@ -17,7 +24,12 @@ class Service implements ConfigInterface
     /**
      * @var string
      */
-    private $host = 'api.frenet.com.br';
+    private $defaultHost = 'api.frenet.com.br';
+
+    /**
+     * @var string
+     */
+    private $host = null;
 
     /**
      * @return string
@@ -28,10 +40,101 @@ class Service implements ConfigInterface
     }
 
     /**
+     * @param string $protocol
+     *
+     * @return $this
+     */
+    public function setProtocol($protocol)
+    {
+        $protocol = trim(strtolower($protocol));
+
+        if ($this->validateProtocol($protocol)) {
+            $this->protocol = $protocol;
+        }
+
+        return $this;
+    }
+
+    /**
      * @return string
      */
     public function getHost()
     {
-        return (string) $this->host;
+        return $this->getHostname();
+    }
+
+    /**
+     * @return string
+     */
+    public function getHostname()
+    {
+        if (empty($this->host)) {
+            return $this->defaultHost;
+        }
+
+        return (string) $this->normalizeHostname($this->host);
+    }
+
+    /**
+     * @param string $host
+     *
+     * @return $this
+     */
+    public function setHostname($host)
+    {
+        if ($this->validateHostname($host)) {
+            $this->host = $this->normalizeHostname($host);
+        }
+
+        return $this;
+    }
+
+    /**
+     * @param string $host
+     *
+     * @return bool
+     */
+    private function validateHostname($host)
+    {
+        $host = $this->normalizeHostname($host);
+
+        if (empty($host)) {
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * @param string $host
+     *
+     * @return string
+     */
+    private function normalizeHostname($host)
+    {
+        $host = trim($host);
+        $host = strtolower($host);
+
+        return $host;
+    }
+
+    /**
+     * @param string $protocol
+     *
+     * @return bool
+     */
+    private function validateProtocol($protocol)
+    {
+        $protocol = strtolower($protocol);
+
+        if (empty($protocol)) {
+            return false;
+        }
+
+        if (!in_array($protocol, $this->validProtocols)) {
+            return false;
+        }
+
+        return true;
     }
 }
