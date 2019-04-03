a74e98b47a75dca13e5b54ea91c0eb116cd01fa4
Tiago Sampaio <tiago@tiagosampaio.com>
2019-04-03 12:32:17 -0300

    Added required phpunit tests.

--- dev/test/unit/Frenet/Config/ServiceTest.php
+++ dev/test/unit/Frenet/Config/ServiceTest.php
@@ -2,6 +2,7 @@
 
 namespace FrenetTest\Config;
 
+use Frenet\Framework\Exception\WrongDataTypeException;
 use FrenetTest\TestCase;
 
 /**
@@ -28,5 +29,57 @@ class ServiceTest extends TestCase
     {
         $this->assertEquals('http', $this->object->getProtocol());
         $this->assertEquals('api.frenet.com.br', $this->object->getHost());
+    }
+
+    /**
+     * @test
+     * @throws WrongDataTypeException
+     */
+    public function customProtocol()
+    {
+        $expectedProtocol = 'https';
+
+        $this->object->setProtocol('https');
+        $this->assertEquals($expectedProtocol, $this->object->getProtocol());
+
+        $this->object->setProtocol('HTTPS');
+        $this->assertEquals($expectedProtocol, $this->object->getProtocol());
+
+        $this->object->setProtocol('HtTpS');
+        $this->assertEquals($expectedProtocol, $this->object->getProtocol());
+    }
+
+    /**
+     * @test
+     * @throws WrongDataTypeException
+     */
+    public function customWrongProtocol()
+    {
+        $this->expectException(WrongDataTypeException::class);
+        $this->expectExceptionMessage('Invalid protocol provided.');
+        $this->object->setProtocol('httpz');
+    }
+
+    /**
+     * @test
+     * @throws WrongDataTypeException
+     */
+    public function customHostname()
+    {
+        $expectedHostname = 'custom.frenet.com.br';
+
+        $this->object->setHostname('custom.frenet.com.br');
+        $this->assertEquals($expectedHostname, $this->object->getHostname());
+    }
+
+    /**
+     * @test
+     * @throws WrongDataTypeException
+     */
+    public function customWrongHostname()
+    {
+        $this->expectException(WrongDataTypeException::class);
+        $this->expectExceptionMessage('Invalid hostname format provided.');
+        $this->object->setHostname('');
     }
 }
